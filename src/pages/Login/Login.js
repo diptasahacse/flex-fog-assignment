7daa87fb85823c16ec88fddeedf03f47c005a761
diptasahacse <diptasaha.lpu.cse@gmail.com>
2022-07-21 02:11:17 +0600

    design login page, get value from input and display it in console

--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -1,9 +1,40 @@
-import React from 'react';
-
+import React, { useRef } from 'react';
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
 const Login = () => {
+    const emailRef = useRef();
+    const passwordRef = useRef();
+
+    const loginHandler = (event) => {
+        event.preventDefault()
+        const email = emailRef.current.value;
+        const password = passwordRef.current.value;
+        
+
+
+    }
+
     return (
-        <div>
-            <h3>Login</h3>
+        <div className='container overflow-hidden'>
+
+            <div className='w-50 mx-auto mt-5'>
+                <h3 className='my-4 fs-3'>Login</h3>
+                <Form onSubmit={loginHandler}>
+                    <Form.Group className="mb-3" controlId="formBasicEmail">
+                        <Form.Label>Email address</Form.Label>
+                        <Form.Control ref={emailRef} type="email" placeholder="Enter email" />
+                    </Form.Group>
+
+                    <Form.Group className="mb-3" controlId="formBasicPassword">
+                        <Form.Label>Password</Form.Label>
+                        <Form.Control ref={passwordRef} type="password" placeholder="Password" />
+                    </Form.Group>
+                    <Button variant="primary" type="submit">
+                        Submit
+                    </Button>
+                </Form>
+            </div>
+
         </div>
     );
 };
